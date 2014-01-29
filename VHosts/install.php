4998330cc286d70d747a65f665392704cbafdadd
pedro <plazanet.pierre@gmail.com>
2014-01-29 13:47:59 +0100

    Update apache config

--- VHosts/install.php
+++ VHosts/install.php
@@ -3,7 +3,7 @@
 
 $dir = dirname(dirname(__DIR__)) . '/Works/';
 $hosts = '/etc/hosts';
-$sites = '/etc/apache2/sites-available/default';
+$sites = '/etc/apache2/sites-available/000-default.conf';
 $limiter = "# CUSTOM HOSTS\n";
 
 // HOST FILE
@@ -53,15 +53,19 @@ $handle = opendir($dir);
 
 while (false !== ($entry = readdir($handle))) {
     if (!in_array($entry, ['.', '..']) && is_dir($dir.$entry)) {
+        $tags = $dir.$entry;
+        if (is_dir($dir.$entry.'/web/')) {
+            $targ = sprintf('%s/web', $targ);
+        }
         $content[] = sprintf("\n");
         $content[] = sprintf("<VirtualHost *:80>\n");
         $content[] = sprintf("\tServerName %s.dev\n", strtolower($entry));
         $content[] = sprintf("\tServerAlias %s.dev\n", $entry);
-        if (is_dir($dir.$entry.'/web/')) {
-            $content[] = sprintf("\tDocumentRoot %s/web\n", $dir.$entry);
-        } else {
-            $content[] = sprintf("\tDocumentRoot %s\n", $dir.$entry);
-        }
+        $content[] = sprintf("\tDocumentRoot %s\n", $targ);
+        $content[] = sprintf("\t<Directory %s>\n", $targ);
+        $content[] = sprintf("\tAllowOverride All\n");
+        $content[] = sprintf("\tRequire all granted\n");
+        $content[] = sprintf("\t</Directory>\n");
         $content[] = sprintf("</VirtualHost>\n");
     }
 }
